ab7dee0988cc715c023b38c287493e9047c9126d
Sai Ganesh Lanka <23a95a6112@aec.edu.in>
2025-12-10 16:53:34 +0530

    Time: 4 ms (37.60%) | Memory: 43.1 MB (45.10%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='['||ch=='{')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(st.isEmpty())
+                {
+                    return false;
+                }
+                char peek=st.peek();
+                if(ch==']' && peek!='[')
+                {
+                    return false;
+                }
+                else if(ch==')' && peek!='(')
+                {
+                    return false;
+                }
+                else if(ch=='}' && peek!='{')
+                {
+                    return false;
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}

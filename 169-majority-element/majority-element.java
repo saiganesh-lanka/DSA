b628ba8d4b386842eae6028aca43a87dd674e368
Sai Ganesh Lanka <23a95a6112@aec.edu.in>
2025-12-10 17:06:48 +0530

    Time: 15 ms (23.05%) | Memory: 52.3 MB (85.57%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+         int s=nums.length/2;
+        for(int n:nums)
+        {
+            hm.put(n,hm.getOrDefault(n,0)+1);
+             if(hm.get(n)>s)
+        {
+              return n;
+        }
+        }
+        return -1;
+
+    }
+}

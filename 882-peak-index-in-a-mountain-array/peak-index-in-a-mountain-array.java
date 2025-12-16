9af06c1e5138ea958da2837b8e9c766531ae2f52
Sai Ganesh Lanka <23a95a6112@aec.edu.in>
2025-12-16 11:49:27 +0530

    Time: 0 ms (100.00%) | Memory: 80.4 MB (9.25%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,12 +1,19 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        for(int i=1;i<arr.length-1;i++)
+        int i=0;
+        int j=arr.length-1;
+        while(i<j)
         {
-            if(arr[i-1]<arr[i] && arr[i]>arr[i+1])
+            int mid=i+(j-i)/2;
+            if(arr[mid]<arr[mid+1])
             {
-                return i;
+                i=mid+1;
+            }
+            else
+            {
+                j=mid;
             }
         }
-        return -1;
+        return i;
     }
 }
